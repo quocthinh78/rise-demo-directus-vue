e11ca926fc8ba4a8a8e9463cbbeb4d97a5856398
quocthinh78 <thinhvq@axpara.com>
2023-02-02 15:39:25 +0700

    update

--- src/components/infomation/InformationList.vue
+++ src/components/infomation/InformationList.vue
@@ -1,12 +1,51 @@
 <template>
-    <div>
-        list
+    <div class="document-wrap">
+        <div
+            class="flex items-center gap-4 max-w-lg p-4 font-normal mb-6 mt-2 bg-[#6C7C94]  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-white">
+            <div class="w-12 h-12 flex justify-center items-center rounded-lg bg-white">
+                <svg class="w-10 h-10  text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
+                        clip-rule="evenodd"></path>
+                    <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
+                </svg>
+            </div>
+            <div>
+                <h5 class="mb-1 text-[16px]">Upload business documents and information</h5>
+                <p class="text-[11px]">Unlock all Rise features now</p>
+            </div>
+        </div>
     </div>
+    <div class="directors-wrap">
+        <div
+            class="flex items-center gap-4 max-w-lg p-4 font-normal mb-6 bg-[#6C7C94]  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-white">
+            <div class="w-12 h-12 flex justify-center items-center rounded-lg bg-white">
+                <svg class="w-10 h-10  text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
+                        clip-rule="evenodd"></path>
+                    <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
+                </svg>
+            </div>
+            <div>
+                <h5 class="mb-1 text-[16px]">Business directors information</h5>
+                <p class="text-[11px]">Unlock all Rise features now</p>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script setup>
 
 </script>
+
+<style lang="css">
+
+</style>
+
 
 <style>
 @tailwind base;
