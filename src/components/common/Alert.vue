ca69e92b522bee0d5634b44e8d40a5d090108159
quocthinh78 <thinhvq@axpara.com>
2023-02-06 10:36:42 +0700

    update UI

---
+++ src/components/common/Alert.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex items-center rounded-lg gap-2 bg-blue-50 mt-8">
+        <div class="w-[4px] h-[48px] rounded-l-lg" :style="{ backgroundColor: bg }"></div>
+        <slot></slot>
+        <div class="ml-3 text-sm font-medium">
+            {{ title }}
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script >
+import { ref } from "vue";
+export default {
+    name: "Badge",
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        color: {
+            type: String,
+            required: true,
+        },
+        bg: {
+            type: String,
+            required: true,
+        }
+    },
+
+}
+</script>
