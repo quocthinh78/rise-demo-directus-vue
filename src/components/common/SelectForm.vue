7cd9032e6582841bcf5a115799bdb6e8df94e0f8
kiethoang <kiethoang611@gmail.com>
2023-02-16 15:33:21 +0700

    update

--- src/components/common/SelectForm.vue
+++ src/components/common/SelectForm.vue
@@ -1,27 +1,38 @@
 <template>
-    <button :id="id" :class="inputClass" type="button" @click="openDropdownList" class="justify-between" :disabled="disabled">
-        <div class="m-0 p-0 flex items-center">
-            <slot name="button" v-bind:selectedValue="selectedValue"></slot>
-        </div>
-        <div class="m-0 p-0 flex items-center" v-if="dropdownIcon">
-            <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-            </svg>
-        </div>
-
-    </button>
-    <!-- list container -->
-    <div :class="`${listContainerClass} ${isDropdownOpen ? 'block' : 'hidden'}`">
-        <!-- list -->
-        <ul :class="listClass" :aria-labelledby="id">
-            <!-- item -->
-            <li v-for="item in listItem" :key="item.value" :class="itemClass" class="cursor-pointer flex items-center" @click="itemClicked(item)" >
-                <slot name="item" v-bind:item="item">
-                    <span class="text-sm leading-5">{{ item.label }}</span>
+    <div class="relative">
+        <button :id="id" :class="inputClass" type="button" @click="openDropdownList" class="justify-between" :disabled="disabled">
+            <template v-if="selectedValue === null && placeholder">
+                <slot name="placeholder">
+                    <span class="font-normal text-sm items-center text-gray-500">{{ placeholder }}</span>
                 </slot>
-            </li>
-        </ul>
+            </template>
+            <template v-else>
+                <div class="m-0 p-0 flex items-center">
+                    <slot name="button" v-bind:selectedValue="selectedValue">
+                        <span class="font-normal text-sm items-center leading-5">{{ selectedValue.label }}</span>
+                    </slot>
+                </div>
+            </template>
+            <div class="m-0 p-0 flex items-center" v-if="dropdownIcon">
+                <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                </svg>
+            </div>
+        </button>
+        <!-- list container -->
+        <div :class="`${listContainerClass} ${isDropdownOpen ? 'block' : 'hidden'}`">
+            <!-- list -->
+            <ul :class="listClass" :aria-labelledby="id">
+                <!-- item -->
+                <li v-for="item in listItem" :key="item.value" :class="itemClass" class="cursor-pointer flex items-center" @click="itemClicked(item)" >
+                    <slot name="item" v-bind:item="item">
+                        <span class="text-sm leading-5">{{ item.label }}</span>
+                    </slot>
+                </li>
+            </ul>
+        </div>
     </div>
+
 </template>
 
 <style scoped>
@@ -68,9 +79,15 @@ export default {
         },
         selectedValue: {
             type: Object,
-            required: true,
-            default: null
-        }
+            default: {}
+        },
+        placeholder: {
+            type: String,
+            default: null,
+        },
+        method: { 
+            type: Function 
+        },
     },
     emits: ['update:selectedValue'],
     data() {
@@ -85,6 +102,7 @@ export default {
         itemClicked(item) {
             this.$emit('update:selectedValue', item)
             this.openDropdownList();
+            // this.method(item, listItem);
         }
     }
 }
