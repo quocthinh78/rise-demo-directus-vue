fadc96c3f61fadf936c6893e4691f382cd92c5e4
kiethoang <kiethoang611@gmail.com>
2023-02-20 11:57:00 +0700

    update

--- src/components/common/UploadFileForm.vue
+++ src/components/common/UploadFileForm.vue
@@ -1,13 +1,12 @@
 <template>
     <label :for="id" :class="labelClass">{{ label }}</label>
     <input type="file" ref="files" :multiple="multiple" :accept="typeAccepted"
-    class="disabled:bg-[var(--background-subdued)] disabled:text-[var(--foreground-subdued)] rounded-lg" 
-    :id="id" :class="inputClass" :placeholder="placeholder" :disabled="disabled" @change="handleFiles"/>
+        class="disabled:bg-[var(--background-subdued)] disabled:text-[var(--foreground-subdued)]
+                    file:rounded-lg file:bg-[var(--fc-button-bg-color)] disabled:file:bg-[var(--v-button-background-color-disabled)] disabled:file:cursor-default" :id="id" :class="inputClass"
+        :placeholder="placeholder" disabled @change="handleFiles" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
 
 <script>
 export default {
@@ -59,20 +58,14 @@ export default {
     },
     methods: {
         handleFiles() {
-            if(this.$refs.files.files.length > 0) {
+            if (this.$refs.files.files.length > 0) {
                 this.fileInput = [];
                 for (let i = 0; i < this.$refs.files.files.length; i++) {
                     this.fileInput.push(this.$refs.files.files[i])
                 }
-                this.$emit('update:modelValue', this.fileInput) 
+                this.$emit('update:modelValue', this.fileInput)
             }
         }
     }
 }
 </script>
-<style scoped>
-input::file-selector-button {
-    background-color: var(--fc-button-bg-color) !important;
-    border-radius: 0.5rem;
-}
-</style>
